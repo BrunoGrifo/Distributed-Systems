2bc5fb6271ed279ed9c00d27c347e33a5295fe65
Grifo <leitaogrifo@gmail.com>
2017-10-13 23:46:35 +0100

    temporary save

--- src/server/TCPServer.java
+++ src/server/TCPServer.java
@@ -5,38 +5,36 @@ import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.io.*;
 import static java.lang.System.*;
+
 
 public class TCPServer{
 	
 	public static String rmiHost;
 	public static int rmiPort;
-	public static String Server;
-	public static int ServerPort;
-	public static String SecundaryServer;
-	public static int SecundaryServerPort;
+	public static String ServerAHost;
+	public static int ServerAPort;
+	public static String ServerBHost;
+	public static int ServerBPort;
 	
-    public static void main(String args[]){
-        int numero=0;
-        ArrayList<Connection> connections = new ArrayList<Connection>();
-        
-        try{
-            int serverPort = 12345;
-            System.out.println("A Escuta no Porto 12345");
-            ServerSocket listenSocket = new ServerSocket(serverPort);
-            System.out.println("LISTEN SOCKET="+listenSocket);
-            while(true) {
-                Socket clientSocket = listenSocket.accept(); // BLOQUEANTE
-                System.out.println("CLIENT_SOCKET (created at accept())="+clientSocket);
-                numero ++;
-                connections.add(new Connection(clientSocket,connections, numero));
-            }
-        }catch(IOException e)
-        {System.out.println("Listen:" + e.getMessage());}
-    }
-    public static void carregaServerConfig(){
+	public static void main(String[] args){
+		carregaServerConfig();
+		//ServerA servidor = new ServerA(serverAHost,serverAPort,rmiHost,rmiPort);
+		Scanner sc1 = new Scanner(System.in);
+		System.out.print("1 - ServerA\n2 - ServerB\n: ");
+		String opc = sc1.nextLine();
+		if(opc.equals("1")){
+			ServerA serverA = new ServerA(ServerAHost,ServerAPort,rmiHost,rmiPort,ServerBHost,ServerBPort);
+		}else if(opc.equals("2")){
+			ServerB serverB = new ServerB(ServerBHost,ServerBPort,rmiHost,rmiPort,ServerAHost,ServerAPort);
+		}
+	}
+	
+	
+	public static void carregaServerConfig(){
 		String file = "ServerAConfig.txt";
 		String line;
 		StringTokenizer tokenizer;
@@ -49,17 +47,17 @@ public class TCPServer{
 				tokenizer = new StringTokenizer(line,"=");
 				line = tokenizer.nextToken();
 				if(line.equals("Server A Host")){
-					serverAHost = tokenizer.nextToken();
+					ServerAHost = tokenizer.nextToken();
 				}else if(line.equals("Server A Port")){
-					serverAPort = Integer.parseInt(tokenizer.nextToken());
+					ServerAPort = Integer.parseInt(tokenizer.nextToken());
 				}else if(line.equals("RMI Host")){
 					rmiHost = tokenizer.nextToken();
 				}else if(line.equals("RMI Port")){
 					rmiPort = Integer.parseInt(tokenizer.nextToken());
 				}else if(line.equals("Server B Host")){
-					serverBHost = tokenizer.nextToken();
+					ServerBHost = tokenizer.nextToken();
 				}else if(line.equals("Server B Port")){
-					serverBPort = Integer.parseInt(tokenizer.nextToken());
+					ServerBPort = Integer.parseInt(tokenizer.nextToken());
 				}
 			}
 		}catch(FileNotFoundException e){
@@ -72,70 +70,88 @@ public class TCPServer{
 
 	}
 }
+class ServerA extends Thread{
+	public String myHost;
+	public int myPort;
+	public String targetHost;
+	public int targetPort;
+	public String rmiHost;
+	public int rmiPort;
+	public Receiver receiver;
+	public MulticastSocket multiSocket;
+	public String group;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//= Thread para tratar de cada canal de comunicação com um cliente
-class Connection extends Thread {
-	BufferedReader inStream = null;
-    PrintWriter outStream;
-    Socket clientSocket;
-    int thread_number;
-    ArrayList<Connection> lista;
-    
-    public Connection (Socket aClientSocket,ArrayList<Connection> connections, int numero) {
-        thread_number = numero;
-        lista=connections;
-        clientSocket=aClientSocket;
-        try{
-	            inStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-	            outStream = new PrintWriter(clientSocket.getOutputStream(), true);
-	            this.start();
-        }catch(IOException e){System.out.println("Connection:" + e.getMessage());}
-    }
-    //=============================
-    public void run(){
-    	//Conecção ao rmi ----->falta
-        try{
-            while(true){
-
-            	readComandLine(inStream.readLine());
-            }
-        }catch(EOFException e){System.out.println("EOF:" + e);
-        }catch(IOException e){System.out.println("IO:" + e);}
-    }
-    
-    public static void readComandLine(String s){
-    	try{
-        	HashMap<String, String> m = ProtocolParser.parse(s);
-        	
-        	switch(m.get("type")) {
-        		case("login"):{
-        			String username = m.get("username").equals(null)?null:m.get("username");
-	    			String password = m.get("password").equals(null)?null:m.get("password");
-	    			
-        		}
-        		case("satus"):{
-        			String logged = m.get("logged").equals(null)?null:m.get("logged");
-        			String msg = m.get("msg").equals(null)?null:m.get("msg");
-        			
-        		}
-        		
-        	}
-        	
-    	}catch(Exception e){ System.out.println("type: error_message, status: not enought arguments");};
-    }
+	public ServerA(String myHost,int myPort,String rmiHost,int rmiPort,String targetHost, int targetPort){
+		this.myHost=myHost;
+		this.myPort=myPort;
+		this.rmiHost=rmiHost;
+		this.rmiPort=rmiPort;
+		this.targetHost=targetHost;
+		this.targetPort=targetPort;
+		//this.receiver = new Receiver(myPort,rmiHost,rmiPort);
+		this.start();
+	}
+	public void run(){
+		/*
+		 * Aqui será feita a comunicação com o servidor B através do protocolo UDP
+		 */
+	}
 }
+
+class ServerB extends Thread{
+	public String myHost;
+	public int myPort;
+	public String targetHost;
+	public int targetPort;
+	public String rmiHost;
+	public int rmiPort;
+	public Receiver receiver;
+	public MulticastSocket multiSocket;
+	public String group;
+
+	public ServerB(String myHost,int myPort,String rmiHost,int rmiPort,String targetHost, int targetPort){
+		this.myHost=myHost;
+		this.myPort=myPort;
+		this.rmiHost=rmiHost;
+		this.rmiPort=rmiPort;
+		this.targetHost=targetHost;
+		this.targetPort=targetPort;
+		//this.receiver = new Receiver(myPort,rmiHost,rmiPort);
+		this.start();
+	}
+	public void run(){
+		/*
+		 * Aqui será feita a comunicação com o servidor A através do protocolo UDP
+		 */
+	}
+}
+class Receiver extends Thread{
+	public int myPort;
+	public String rmiHost;
+	public int rmiPort;
+	public int numero = 0;
+	
+	public Receiver(int myPort,String rmiHost,int rmiPort){
+		this.myPort=myPort;
+		this.rmiHost=rmiHost;
+		this.rmiPort=rmiPort;
+		this.start();
+	}
+	public void run(){
+		int thread_id=0;
+		try{
+			ServerSocket listenSocket = new ServerSocket(myPort);
+			System.out.println("Listen Socket="+listenSocket);
+			while(true){
+				Socket clientSocket = listenSocket.accept();
+				System.out.println("Client_socket accepted= "+clientSocket);
+				listaSockets.add(clientSocket);
+				new Connection(clientSocket,rmiHost,rmiPort);
+			}
+		}catch(IOException e){
+			System.out.println("Listen: "+e.getMessage());
+		}
+	}
+
+}
+	
